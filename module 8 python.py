51be3f13d9e4cda481506fd093263e7a195d9252
tthu-pixel <tthu@ivytech.edu>
2025-12-17 16:34:07 -0500

    Add files via upload

---
+++ module 8 python.py
@@ -0,0 +1,18 @@
+
+def greater_than(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+a = 2
+b = 3
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+print()
+
+a = 10
+b = 6
+result = greater_than(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
